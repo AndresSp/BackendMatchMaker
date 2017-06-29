d01e5b2c38a1910e32832a0453a1987ee026d015
AndresSp <andrestunonsp@gmail.com>
2017-06-29 12:34:44 -0500

    Se incluyó los spUserLikes al mapeo, probando git desde VS

--- MatchMaker.Infrastructure/Data/Touchdb.Context.cs
+++ MatchMaker.Infrastructure/Data/Touchdb.Context.cs
@@ -161,5 +161,205 @@ namespace MatchMaker.Infrastructure.Data
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_BooksRegister_Result>("sp_User_BooksRegister", userIdParameter, genreIdParameter);
         }
+    
+        public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            var versionParameter = version.HasValue ?
+                new ObjectParameter("version", version) :
+                new ObjectParameter("version", typeof(int));
+    
+            var definitionParameter = definition != null ?
+                new ObjectParameter("definition", definition) :
+                new ObjectParameter("definition", typeof(byte[]));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_alterdiagram", diagramnameParameter, owner_idParameter, versionParameter, definitionParameter);
+        }
+    
+        public virtual int sp_creatediagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            var versionParameter = version.HasValue ?
+                new ObjectParameter("version", version) :
+                new ObjectParameter("version", typeof(int));
+    
+            var definitionParameter = definition != null ?
+                new ObjectParameter("definition", definition) :
+                new ObjectParameter("definition", typeof(byte[]));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_creatediagram", diagramnameParameter, owner_idParameter, versionParameter, definitionParameter);
+        }
+    
+        public virtual int sp_dropdiagram(string diagramname, Nullable<int> owner_id)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_dropdiagram", diagramnameParameter, owner_idParameter);
+        }
+    
+        public virtual ObjectResult<sp_helpdiagramdefinition_Result> sp_helpdiagramdefinition(string diagramname, Nullable<int> owner_id)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_helpdiagramdefinition_Result>("sp_helpdiagramdefinition", diagramnameParameter, owner_idParameter);
+        }
+    
+        public virtual ObjectResult<sp_helpdiagrams_Result> sp_helpdiagrams(string diagramname, Nullable<int> owner_id)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_helpdiagrams_Result>("sp_helpdiagrams", diagramnameParameter, owner_idParameter);
+        }
+    
+        public virtual int sp_renamediagram(string diagramname, Nullable<int> owner_id, string new_diagramname)
+        {
+            var diagramnameParameter = diagramname != null ?
+                new ObjectParameter("diagramname", diagramname) :
+                new ObjectParameter("diagramname", typeof(string));
+    
+            var owner_idParameter = owner_id.HasValue ?
+                new ObjectParameter("owner_id", owner_id) :
+                new ObjectParameter("owner_id", typeof(int));
+    
+            var new_diagramnameParameter = new_diagramname != null ?
+                new ObjectParameter("new_diagramname", new_diagramname) :
+                new ObjectParameter("new_diagramname", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_renamediagram", diagramnameParameter, owner_idParameter, new_diagramnameParameter);
+        }
+    
+        public virtual int sp_upgraddiagrams()
+        {
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("sp_upgraddiagrams");
+        }
+    
+        public virtual ObjectResult<sp_User_BooksSelect_Result> sp_User_BooksSelect(Nullable<int> matchId)
+        {
+            var matchIdParameter = matchId.HasValue ?
+                new ObjectParameter("MatchId", matchId) :
+                new ObjectParameter("MatchId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_BooksSelect_Result>("sp_User_BooksSelect", matchIdParameter);
+        }
+    
+        public virtual ObjectResult<sp_User_EntertainmentRegister_Result> sp_User_EntertainmentRegister(Nullable<System.Guid> userId, Nullable<int> genreId)
+        {
+            var userIdParameter = userId.HasValue ?
+                new ObjectParameter("UserId", userId) :
+                new ObjectParameter("UserId", typeof(System.Guid));
+    
+            var genreIdParameter = genreId.HasValue ?
+                new ObjectParameter("GenreId", genreId) :
+                new ObjectParameter("GenreId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_EntertainmentRegister_Result>("sp_User_EntertainmentRegister", userIdParameter, genreIdParameter);
+        }
+    
+        public virtual ObjectResult<sp_User_EntertainmentSelect_Result> sp_User_EntertainmentSelect(Nullable<int> matchId)
+        {
+            var matchIdParameter = matchId.HasValue ?
+                new ObjectParameter("MatchId", matchId) :
+                new ObjectParameter("MatchId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_EntertainmentSelect_Result>("sp_User_EntertainmentSelect", matchIdParameter);
+        }
+    
+        public virtual ObjectResult<sp_User_ExpArtsRegister_Result> sp_User_ExpArtsRegister(Nullable<System.Guid> userId, Nullable<int> genreId)
+        {
+            var userIdParameter = userId.HasValue ?
+                new ObjectParameter("UserId", userId) :
+                new ObjectParameter("UserId", typeof(System.Guid));
+    
+            var genreIdParameter = genreId.HasValue ?
+                new ObjectParameter("GenreId", genreId) :
+                new ObjectParameter("GenreId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_ExpArtsRegister_Result>("sp_User_ExpArtsRegister", userIdParameter, genreIdParameter);
+        }
+    
+        public virtual ObjectResult<sp_User_ExpArtsSelect_Result> sp_User_ExpArtsSelect(Nullable<int> matchId)
+        {
+            var matchIdParameter = matchId.HasValue ?
+                new ObjectParameter("MatchId", matchId) :
+                new ObjectParameter("MatchId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_ExpArtsSelect_Result>("sp_User_ExpArtsSelect", matchIdParameter);
+        }
+    
+        public virtual ObjectResult<sp_User_MusicRegister_Result> sp_User_MusicRegister(Nullable<System.Guid> userId, Nullable<int> genreId)
+        {
+            var userIdParameter = userId.HasValue ?
+                new ObjectParameter("UserId", userId) :
+                new ObjectParameter("UserId", typeof(System.Guid));
+    
+            var genreIdParameter = genreId.HasValue ?
+                new ObjectParameter("GenreId", genreId) :
+                new ObjectParameter("GenreId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_MusicRegister_Result>("sp_User_MusicRegister", userIdParameter, genreIdParameter);
+        }
+    
+        public virtual ObjectResult<sp_User_MusicSelect_Result> sp_User_MusicSelect(Nullable<int> matchId)
+        {
+            var matchIdParameter = matchId.HasValue ?
+                new ObjectParameter("MatchId", matchId) :
+                new ObjectParameter("MatchId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_MusicSelect_Result>("sp_User_MusicSelect", matchIdParameter);
+        }
+    
+        public virtual ObjectResult<sp_User_SportRegister_Result> sp_User_SportRegister(Nullable<System.Guid> userId, Nullable<int> genreId)
+        {
+            var userIdParameter = userId.HasValue ?
+                new ObjectParameter("UserId", userId) :
+                new ObjectParameter("UserId", typeof(System.Guid));
+    
+            var genreIdParameter = genreId.HasValue ?
+                new ObjectParameter("GenreId", genreId) :
+                new ObjectParameter("GenreId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_SportRegister_Result>("sp_User_SportRegister", userIdParameter, genreIdParameter);
+        }
+    
+        public virtual ObjectResult<sp_User_SportSelect_Result> sp_User_SportSelect(Nullable<int> matchId)
+        {
+            var matchIdParameter = matchId.HasValue ?
+                new ObjectParameter("MatchId", matchId) :
+                new ObjectParameter("MatchId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<sp_User_SportSelect_Result>("sp_User_SportSelect", matchIdParameter);
+        }
     }
 }
--- MatchMaker.Infrastructure/Repository/TouchRepository.cs
+++ MatchMaker.Infrastructure/Repository/TouchRepository.cs
@@ -121,5 +121,6 @@ namespace MatchMaker.Infrastructure.Repository
             }
         }
         #endregion
+
     }
 }
