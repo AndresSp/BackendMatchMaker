5e397ba7d53ed4da2c1da341d18428bc9d5a0c56
AndresSp <andrestunonsp@gmail.com>
2017-07-01 13:36:12 -0500

    Se agregó PrologLogic y se corrigió Loader de Prolog

--- MatchMaker.Infrastructure.Prolog/Loader.cs
+++ MatchMaker.Infrastructure.Prolog/Loader.cs
@@ -10,59 +10,59 @@ namespace MatchMaker.Infrastructure.Prolog
     {
         TouchRepository touchRep = new Repository.TouchRepository();
 
-        private List<Guid> LoadUserIdList()
+        private List<string> LoadUserIdList()
         {
-            List<Guid> userIdList = new List<Guid>(); 
+            List<string> userIdList = new List<string>(); 
             foreach(sp_UserSelect_Result user in touchRep.UserMasterSelect())
-                userIdList.Add(user.UserId);
+                userIdList.Add(user.UserId.ToString());
             return userIdList;
         }
 
-        public Dictionary<Guid, string> LoadUserBookLikes()
+        public Dictionary<string, string> LoadUserBookLikes()
         {
-            Dictionary<Guid, string> UserBookLikes = new Dictionary<Guid, string>(); //ID1,Misterio
+            Dictionary<string, string> UserBookLikes = new Dictionary<string, string>(); //ID1,Misterio
 
-            foreach (Guid userid in LoadUserIdList())
+            foreach (string userid in LoadUserIdList())
                 foreach( sp_GetUserBookLikes_Result userlike in touchRep.GetUserBookLikes(userid.ToString()))
                     UserBookLikes.Add(userid, userlike.Name);
             return UserBookLikes;
         }
 
-        public Dictionary<Guid, string> LoadUserEntertainmentLikes()
+        public Dictionary<string, string> LoadUserEntertainmentLikes()
         {
-            Dictionary<Guid, string> UserEntertainmentLikes = new Dictionary<Guid, string>(); //ID1,Misterio
+            Dictionary<string, string> UserEntertainmentLikes = new Dictionary<string, string>(); //ID1,Misterio
 
-            foreach (Guid userid in LoadUserIdList())
+            foreach (string userid in LoadUserIdList())
                 foreach (sp_GetUserEntertainmentLikes_Result userlike in touchRep.GetUserEntertainmentLikes(userid.ToString()))
                     UserEntertainmentLikes.Add(userid, userlike.Name);
             return UserEntertainmentLikes;
         }
 
-        public Dictionary<Guid, string> LoadUserExpArtsLikes()
+        public Dictionary<string, string> LoadUserExpArtsLikes()
         {
-            Dictionary<Guid, string> UserExpArtsLikes = new Dictionary<Guid, string>(); //ID1,Misterio
+            Dictionary<string, string> UserExpArtsLikes = new Dictionary<string, string>(); //ID1,Misterio
 
-            foreach (Guid userid in LoadUserIdList())
+            foreach (string userid in LoadUserIdList())
                 foreach (sp_GetUserExpArtsLikes_Result userlike in touchRep.GetUserExpArtsLikes(userid.ToString()))
                     UserExpArtsLikes.Add(userid, userlike.Name);
             return UserExpArtsLikes;
         }
 
-        public Dictionary<Guid, string> LoadUserMusicLikes()
+        public Dictionary<string, string> LoadUserMusicLikes()
         {
-            Dictionary<Guid, string> UserMusicLikes = new Dictionary<Guid, string>(); //ID1,Misterio
+            Dictionary<string, string> UserMusicLikes = new Dictionary<string, string>(); //ID1,Misterio
 
-            foreach (Guid userid in LoadUserIdList())
+            foreach (string userid in LoadUserIdList())
                 foreach (sp_GetUserMusicLikes_Result userlike in touchRep.GetUserMusicLikes(userid.ToString()))
                     UserMusicLikes.Add(userid, userlike.Name);
             return UserMusicLikes;
         }
 
-        public Dictionary<Guid, string> LoadUserSportLikes()
+        public Dictionary<string, string> LoadUserSportLikes()
         {
-            Dictionary<Guid, string> UserSportLikes = new Dictionary<Guid, string>(); //ID1,Misterio
+            Dictionary<string, string> UserSportLikes = new Dictionary<string, string>(); //ID1,Misterio
 
-            foreach (Guid userid in LoadUserIdList())
+            foreach (string userid in LoadUserIdList())
                 foreach (sp_GetUserSportLikes_Result userlike in touchRep.GetUserSportLikes(userid.ToString()))
                     UserSportLikes.Add(userid, userlike.Name);
             return UserSportLikes;
--- MatchMaker.Infrastructure.Prolog/PrologLogic.cs
+++ MatchMaker.Infrastructure.Prolog/PrologLogic.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Prolog;
 
 namespace MatchMaker.Infrastructure.Prolog
 {
     class PrologLogic
     {
+        Loader loader = new Loader();
+        PrologEngine prolog = new PrologEngine();
+
+        //public void 
+
+        private void LoadLikesToProlog()
+        {
+            foreach (KeyValuePair<string, string> userlike in loader.LoadUserBookLikes())
+                prolog.ConsultFromString(string.Format("likeBooks({0},{1}).", userlike.Key, userlike.Value));
+
+            foreach (KeyValuePair<string, string> userlike in loader.LoadUserEntertainmentLikes())
+                prolog.ConsultFromString(string.Format("likeEntertainment({0},{1}).", userlike.Key, userlike.Value));
+
+            foreach (KeyValuePair<string, string> userlike in loader.LoadUserExpArtsLikes())
+                prolog.ConsultFromString(string.Format("likeExpArts({0},{1}).", userlike.Key, userlike.Value));
+
+            foreach (KeyValuePair<string, string> userlike in loader.LoadUserMusicLikes())
+                prolog.ConsultFromString(string.Format("likeMusic({0},{1}).", userlike.Key, userlike.Value));
+
+            foreach (KeyValuePair<string, string> userlike in loader.LoadUserSportLikes())
+                prolog.ConsultFromString(string.Format("likeSport({0},{1}).", userlike.Key, userlike.Value));
+        }
+
+
+
+
     }
 }
